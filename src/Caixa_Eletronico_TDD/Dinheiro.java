9b59fa134b3544bee7d7cf4b86bb9ea4e4582d7c
Raphael Vitorino <raphael.silva@catolicasc.org.br>
2016-02-28 19:49:30 -0300

    Subir arquivos do exercicio de TDD

---
+++ src/Caixa_Eletronico_TDD/Dinheiro.java
@@ -0,0 +1,14 @@
+package Caixa_Eletronico_TDD;
+
+public class Dinheiro {
+	private int valor;
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+}
